38d60cbfbbba20df61bf1e8673be1fa28c66d77b
natalyasimmons <ndsimmons1@ualr.edu>
2023-05-07 12:38:14 +0000

    Reflection and progressive enhancement added

---
+++ slick-demo/assests/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 600,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
